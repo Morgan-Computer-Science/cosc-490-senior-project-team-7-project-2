0227f3f2c921ef619eb155992201de650db56c02
338_Work <elbal1@morgan.edu>
2026-02-21 01:02:21 -0500

    Update_loginPage_1

---
+++ sentinel-app/src/pages/login.jsx
@@ -0,0 +1,16 @@
+import "./login.css";
+
+const Login = () =>
+    {
+        return (
+            <div className="page">
+                <div className="container">
+                    <h1 className="heading">SENTINEL</h1>
+                    <h2 className="subHeading">Strategic Executive Network for Threat Evaluation and National Leadership</h2>
+                </div>
+            </div>
+        );
+        
+    };
+
+    export default Login;
